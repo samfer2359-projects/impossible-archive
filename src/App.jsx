00ceddade64c47a6f9f21e0527c1c77ed69d9928
Samantha Fernandes <samfer2359@gmail.com>
2026-06-24 18:30:49 +0530

    Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
 import HomePage from "./pages/HomePage";
 import ArchiveListPage from "./pages/ArchiveListPage";
 import ArchiveDetailPage from "./pages/ArchiveDetailPage";
 import VideoBackground from "./components/VideoBackground";
 
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname,
+    });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
-    <VideoBackground />
+      <AnalyticsTracker />
+
+      <VideoBackground />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/archive" element={<ArchiveListPage />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from "react-ga4";
 
 import "./styles.css";
 import App from './App.jsx'
+
+ReactGA.initialize("G-G0QXYMBJ91");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
